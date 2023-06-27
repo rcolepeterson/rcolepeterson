948da77ed6a7866b9b4c84b0bc1185c3f38ec6ea
Cole Peterson <rcolepeterson@gmail.com>
2023-06-27 10:24:22 -0700

    back button, updated proto desc

--- src/app/proto/page.tsx
+++ src/app/proto/page.tsx
@@ -18,6 +18,25 @@ const HomePage: NextPage = () => {
         <div className="mx-auto max-w-screen-xl py-16">
           <Prototypes prototypes={prototypes} />
           <p className=""> and a ton more ...</p>
+          <Link href="/">
+            <div className="text-2xl flex items-center py-8 gap-x-2 font-bold">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+                />
+              </svg>
+              BACK
+            </div>
+          </Link>
         </div>
       </div>
     </section>
