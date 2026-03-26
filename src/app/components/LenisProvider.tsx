d90e6fef731e53f6c164ad534adba1956e5095cd
Cole Peterson <rcolepeterson@gmail.com>
2026-03-26 12:40:31 -0700

    Update Lenis dependency to version 1.3.21 and refactor LenisProvider for improved smooth scrolling

--- src/app/components/LenisProvider.tsx
+++ src/app/components/LenisProvider.tsx
@@ -1,28 +1,21 @@
 "use client";
 
-import { useEffect } from "react";
-import Lenis from "lenis";
+import { ReactLenis } from "lenis/react";
 
 export default function LenisProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  useEffect(() => {
-    const lenis = new Lenis({
-      lerp: 0.1,
-    });
-
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
-
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
-
-  return <>{children}</>;
+  return (
+    <ReactLenis
+      root
+      options={{
+        lerp: 0.07,
+        smoothWheel: true,
+      }}
+    >
+      {children}
+    </ReactLenis>
+  );
 }
