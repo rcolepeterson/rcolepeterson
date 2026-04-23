c0a5c6595df73c1e8937703ba8b3f85e7967bba1
Cole Peterson <rcolepeterson@gmail.com>
2026-04-23 11:42:13 -0700

    Remove changeFrequency and priority fields from sitemap entries

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,14 +5,10 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://rcolepeterson.com",
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
     },
     {
       url: "https://rcolepeterson.com/proto",
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
     },
   ];
 }
